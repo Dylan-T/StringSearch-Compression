a2fe9b7fc3e78d7130797612376354336509d79c
Dylan <thomsodyla1@myvuw.ac.nz>
2018-10-06 10:08:23 +1300

    Knuth Morris Pratt algorithm completed and working.

--- src/KMP.java
+++ src/KMP.java
@@ -7,7 +7,7 @@
 public class KMP {
 
 	public KMP(String pattern, String text) {
-		// TODO maybe fill this in.
+		search(pattern, text);
 	}
 
 	/**
@@ -17,7 +17,53 @@ public class KMP {
 	 * exists, or -1 if it doesn't.
 	 */
 	public int search(String pattern, String text) {
-		// TODO fill this in.
+		int[] matchTable = buildTable(text);
+		char[] patternArr = pattern.toCharArray();
+		char[] textArr = text.toCharArray();
+		int k = 0;
+		int i = 0;
+		int n = text.length();
+		int m = pattern.length();
+		
+		while(k+i < n) {
+			if(patternArr[i] == textArr[k+i]) {
+				i++;
+				if(i == m) {
+					return k;
+				}
+			}else if(matchTable[i] == -1) {
+				i = 0;
+				k += i + 1;
+			}else {
+				k += i - matchTable[i];
+				i = matchTable[i];
+			}
+		}
 		return -1;
+	}
+	
+	public int[] buildTable(String text) {
+		char[] textArr = text.toCharArray();
+		int m = text.length();
+		int[] matchTable = new int[m];
+		matchTable[0] = -1;
+		matchTable[1] = 0;
+		int j = 0;
+		int pos = 2;
+		
+		while(pos < m) {
+			if(textArr[pos-1] == textArr[j]) {
+				matchTable[pos] = j+1;
+				pos++;
+				j++;
+			}else if(j < 0) {
+				j = matchTable[j];
+			}else {
+				matchTable[pos] = 0;
+				pos++;
+			}
+		}
+		
+		return matchTable;
 	}
 }
